6ac694f112b319b74c504461ebc1107914f2df16
Susana Elizabeth Almaraz <sealmaraz@yahoo.com.ar>
2023-12-18 00:49:19 -0300

    [FEAT] method-override and body-parsers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const methodOverride = require('method-override');
+const path = require('path');
 //Montar el server
 const app =express(); //aquí ejecuto express y guardo en una vble para poder tener acceso alos métodos
+
 //asignar el puerto
 const PORT=4000;
 //llamo al mainroutes
@@ -11,6 +14,9 @@ const authRoutes = require(__dirname+'/src/routes/auth.routes');
 
 //acceso a carpeta public
 app.use(express.static('public'));
+app.use(express.urlencoded()); // al enviar info por body lo transfora o traduce a javascript
+app.use(express.json());// al enviar info por body lo transfora o traduce a javascript
+app.use(methodOverride('_method')); //desde los formularios puedo definir el método a utilizar adjuntando en la ruta ?_method=PUT
 
 //app.use(express.urlencoded);
 //probar el servidor
